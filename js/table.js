144d4448d4b77259df572dbc7e3495d9bcba056e
Jeffery Owen <owen.je@husky.neu.edu>
2020-11-10 12:59:02 -0500

    Jeffery Commit 2

--- js/table.js
+++ js/table.js
@@ -1,8 +1,8 @@
 /* global D3 */
 
-// Initialize a scatterplot. Modeled after Mike Bostock's
+// Initialize a line chart. Modeled after Mike Bostock's
 // Reusable Chart framework https://bost.ocks.org/mike/chart/
-function scatterplot() {
+function linechart() {
 
   // Based on Mike Bostock's margin convention
   // https://bl.ocks.org/mbostock/3019563
@@ -10,7 +10,7 @@ function scatterplot() {
       top: 60,
       left: 50,
       right: 30,
-      bottom: 20
+      bottom: 35
     },
     width = 500 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom,
@@ -19,7 +19,7 @@ function scatterplot() {
     xLabelText = '',
     yLabelText = '',
     yLabelOffsetPx = 0,
-    xScale = d3.scaleLinear(),
+    xScale = d3.scalePoint(),
     yScale = d3.scaleLinear(),
     ourBrush = null,
     selectableElements = d3.select(null),
@@ -31,7 +31,7 @@ function scatterplot() {
     let svg = d3.select(selector)
       .append('svg')
         .attr('preserveAspectRatio', 'xMidYMid meet')
-        .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
+        .attr('viewBox', [-500, -500, 1000, 1000].join(' '))
         .classed('svg-content', true);
 
     svg = svg.append('g')
@@ -39,10 +39,7 @@ function scatterplot() {
 
     //Define scales
     xScale
-      .domain([
-        d3.min(data, d => xValue(d)),
-        d3.max(data, d => xValue(d))
-      ])
+      .domain(d3.group(data, xValue).keys())
       .rangeRound([0, width]);
 
     yScale
@@ -52,16 +49,25 @@ function scatterplot() {
       ])
       .rangeRound([height, 0]);
 
+    // X axis
     let xAxis = svg.append('g')
         .attr('transform', 'translate(0,' + (height) + ')')
         .call(d3.axisBottom(xScale));
+        
+    // Put X axis tick labels at an angle
+    xAxis.selectAll('text')	
+        .style('text-anchor', 'end')
+        .attr('dx', '-.8em')
+        .attr('dy', '.15em')
+        .attr('transform', 'rotate(-65)');
         
     // X axis label
     xAxis.append('text')        
         .attr('class', 'axisLabel')
         .attr('transform', 'translate(' + (width - 50) + ',-10)')
         .text(xLabelText);
-      
+    
+    // Y axis and label
     let yAxis = svg.append('g')
         .call(d3.axisLeft(yScale))
       .append('text')
@@ -69,48 +75,56 @@ function scatterplot() {
         .attr('transform', 'translate(' + yLabelOffsetPx + ', -12)')
         .text(yLabelText);
 
+    // Add the line
+    svg.append('path')
+        .datum(data)
+        .attr('class', 'linePath')
+        .attr('d', d3.line()
+          // Just add that to have a curve instead of segments
+          .x(X)
+          .y(Y)
+        );
+
     // Add the points
     let points = svg.append('g')
-      .selectAll('.scatterPoint')
+      .selectAll('.linePoint')
         .data(data);
-
+    
     points.exit().remove();
-
+          
     points = points.enter()
       .append('circle')
-        .attr('class', 'point scatterPoint')
+        .attr('class', 'point linePoint')
       .merge(points)
         .attr('cx', X)
-        .attr('cy', Y)
-        .attr('r', 5);
-    
+        .attr('cy', Y)        
+        .attr('r',5);
+        
     selectableElements = points;
-    
+
     svg.call(brush);
 
     // Highlight points when brushed
     function brush(g) {
-      const brush = d3.brush() // Create a 2D interactive brush
-        .on('start brush', highlight) // When the brush starts/continues do...
-        .on('end', brushEnd) // When the brush ends do...
+      const brush = d3.brush()
+        .on('start brush', highlight)
+        .on('end', brushEnd)
         .extent([
           [-margin.left, -margin.bottom],
           [width + margin.right, height + margin.top]
         ]);
-        
+
       ourBrush = brush;
 
       g.call(brush); // Adds the brush to this element
 
-      // Highlight the selected circles
+      // Highlight the selected circles.
       function highlight(event, d) {
         if (event.selection === null) return;
         const [
           [x0, y0],
           [x1, y1]
         ] = event.selection;
-
-        // If within the bounds of the brush, select it
         points.classed('selected', d =>
           x0 <= X(d) && X(d) <= x1 && y0 <= Y(d) && Y(d) <= y1
         );
@@ -118,11 +132,11 @@ function scatterplot() {
         // Get the name of our dispatcher's event
         let dispatchString = Object.getOwnPropertyNames(dispatcher._)[0];
 
-        // Let other charts know about our selection
+        // Let other charts know
         dispatcher.call(dispatchString, this, svg.selectAll('.selected').data());
       }
       
-      function brushEnd(event, d){
+      function brushEnd(event, d) {
         // We don't want infinite recursion
         if(event.sourceEvent !== undefined && event.sourceEvent.type!='end'){
           d3.select(this).call(brush.move, null);
@@ -207,8 +221,7 @@ function scatterplot() {
     selectableElements.classed('selected', d =>
       selectedData.includes(d)
     );
-
   };
-
+  
   return chart;
 }
