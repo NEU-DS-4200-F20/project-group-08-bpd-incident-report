5a913784e063e16b79a780e0bf7b9de90b5ee381
gbarker2000 <gbarker2000@comcast.net>
2020-11-16 15:30:38 -0500

    trying to add pie chart

--- js/visualization1.js
+++ js/visualization1.js
@@ -1,17 +1,11 @@
-// Immediately Invoked Function Expression to limit access to our 
-// variables and prevent 
+// Immediately Invoked Function Expression to limit access to our
+// variables and prevent
 ((() => {
 
   // Load the data from a json file (you can make these using
   // JSON.stringify(YOUR_OBJECT), just remove the surrounding '')
   d3.csv("./data/officers.csv", function(d) {
   	return {
-      title : d.title,
-      doa : d.doa,
-      rank : d.rank,
-   		ia_score : +d.ia_score,
-    	total : +d.total,
-    	overtime : +d.overtime,
       ia_sustained_allegations : +d.ia_sustained_allegations
   	};
 	}).then(data => {
@@ -21,31 +15,44 @@
     // https://github.com/d3/d3-dispatch
     const dispatchString = 'selectionUpdated';
 
-    // Create a scatterplot given x and y attributes, labels, offsets; 
-    // a dispatcher (d3-dispatch) for selection events; 
-    // a div id selector to put our svg in; and the data to use.
-    let sp1 = scatterplot()
-      .x(d => d.ia_score)
-      .xLabel('IA SCORE')
-      .y(d => d.total)
-      .yLabel('TOTAL SALARY')
-      .yLabelOffset(40)
-      .selectionDispatcher(d3.dispatch(dispatchString))
-      ('#vis-svg-1', data);
 
+    var width = 450
+      height = 450
+      margin = 40
 
+// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+    var radius = Math.min(width, height) / 2 - margin
 
-    
+// append the svg object to the div called 'my_dataviz'
+    var svg = d3.select("#vis1pie")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
+// set the color scale
+    var color = d3.scaleOrdinal()
+      .domain(data)
+      .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
+
+// Compute the position of each group on the pie:
+    var pie = d3.pie()
+      .value(function(d) {return d.value; })
+
+// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    svg.selectAll('svg')
+      .data(data)
+      .enter()
+      .append('path')
+      .attr('d', d3.arc()
+      .innerRadius(0)
+      .outerRadius(radius)
+    )
+      .attr('fill', function(d){ return(color(d.data)) })
+      .attr("stroke", "black")
+      .style("stroke-width", "2px")
+      .style("opacity", 0.7)
   });
 
 })());
-
-
-
-
-
-
-
-
-
